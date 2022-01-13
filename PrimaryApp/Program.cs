dff6c56b93cf0bc7d8e770be5a02574806843275
José Espinoza Bone <megamanx@outlook.com>
2022-01-13 16:20:24 -0500

    Code

---
+++ PrimaryApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+public class Program
+{
+    public async static Task Main(string[] args)
+    {
+        Console.WriteLine("██ PrimaryApp Started");
+        var secondaryAppExeFilename = Path.GetFullPath("SecondaryApp/SecondaryApp.exe");
+        var settingsFilename = Path.GetFullPath("appsettings.json"); // var settingsFilename = Path.GetFullPath("C:\appsettings.json");
+        ProcessStartInfo info = new ProcessStartInfo();
+        info.FileName = secondaryAppExeFilename;
+        info.Arguments = $"--SettingsFile={settingsFilename}";
+        info.WindowStyle = ProcessWindowStyle.Normal;
+        Process process = new Process();
+        process.StartInfo = info;
+        process.Start();
+        await process.WaitForExitAsync();
+        Console.ReadLine();
+        Console.WriteLine("█ PrimaryApp Exited");
+    }
+}
---
+++ SecondaryApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("██ SecondaryApp Started");
+        string settingsFile = args.Where(a => a.Contains("SettingsFile")).First().Split("=")[1];
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile(settingsFile, optional: false, reloadOnChange: true);
+        IConfiguration config = builder.Build();
+        Console.WriteLine($"SettingsFile: {settingsFile}");
+        Console.WriteLine($"AppName value: {config.GetValue<string>("AppName")}");
+        Console.ReadLine();
+        Console.WriteLine("█SecondaryApp Exited");
+    }
+}
